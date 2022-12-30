3818c0fed51622301a10b07679d4d0aa862f0c01
virajpatil8181 <120187995+virajpatil8181@users.noreply.github.com>
2022-12-31 00:03:14 +0530

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    cout << "Hello world!"<<endl;
+
+    return 0;
+}
